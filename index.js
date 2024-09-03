1e9d169cf61ce58d7248ba5547829264f9f067d3
griminir <viktor.elliot.king@gmail.com>
2024-09-03 11:46:26 +0200

    set up the express server and the config

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const config = require('./config');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(config.port, () => {
+  console.log(`listening on http://localhost:${config.port}...`);
+});
